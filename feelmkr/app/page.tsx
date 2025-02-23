4a84fbc6e08db38d096d16f73357bd25dc948f9d
mchanfiou <chanfioumoustapha@gmail.com>
2025-02-23 16:05:09 +0100

    page vitrine front

--- feelmkr/app/page.tsx
+++ feelmkr/app/page.tsx
@@ -1,21 +1,23 @@
-import { Container, Button, Navbar, Nav } from 'react-bootstrap';
+'use client';
+
+import { Container, Button, Navbar, Nav } from 'reactstrap'; // Importation de reactstrap
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
-      <Navbar bg="light" expand="lg" className="p-3">
-        <Navbar.Brand href="#">FeelMKR</Navbar.Brand>
+      <Navbar color="light" light expand="lg" className="p-3">
+        <a href="#" className="navbar-brand">FeelMKR</a> {/* Remplacer Navbar.Brand par une balise a */}
         <Nav className="ms-auto">
-          <Button variant="outline-secondary" className="me-2">Se connecter</Button>
-          <Button variant="dark">Essayer gratuitement</Button>
+          <Button outline color="secondary" className="me-2">Se connecter</Button>
+          <Button color="dark">Essayer gratuitement</Button>
         </Nav>
       </Navbar>
       
       <Container className="text-center my-5">
         <h1 className="fw-bold">Capturez l’instant, <br /> racontez votre histoire</h1>
         <p>FeelMKR, votre vitrine professionnelle pour sublimer vos réalisations vidéo</p>
-        <Button variant="dark" size="lg">Essayer gratuitement</Button>
+        <Button color="dark" size="lg">Essayer gratuitement</Button>
       </Container>
       
       <Container className="text-center">

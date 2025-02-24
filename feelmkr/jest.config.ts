17a296783a749d883d13a5f1868459f4eeb1f6c1
mchanfiou <chanfioumoustapha@gmail.com>
2025-02-24 23:37:13 +0100

    vercel

---
+++ feelmkr/jest.config.ts
@@ -0,0 +1,20 @@
+import nextJest from 'next/jest';
+import type { Config } from 'jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
+
+export default createJestConfig(customJestConfig);

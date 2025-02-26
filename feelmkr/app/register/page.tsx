63bb226caa7fab7546b7cc783b6b0ecb105725ff
mchanfiou <chanfioumoustapha@gmail.com>
2025-02-26 07:27:17 +0100

    ok

--- feelmkr/app/register/page.tsx
+++ feelmkr/app/register/page.tsx
@@ -29,7 +29,7 @@ export default function RegisterPage() {
     }
 
     try {
-      const response = await fetch('http://localhost:8000/utilisateurs/', {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/utilisateurs/`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ company_name: companyName, email, password })
